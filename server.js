a51caad0c56493aac2d46cc1f9c4dd5091e9c457
Khizer Kalki <khizer@kalkiai.com>
2024-09-18 20:05:21 +0530

    some more routes for fire,goals,contributions,investments added

--- server.js
+++ server.js
@@ -367,6 +367,273 @@ app.delete("/budget-overview/:id", authMiddleware, async (req, res) => {
   }
 });
 
+app.get("/fire-calculator/:userId", authMiddleware, async (req, res) => {
+  try {
+    const fireCalculator = await FireCalculator.findOne({
+      where: { userId: req.params.userId },
+    });
+    res.status(200).json(fireCalculator);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve data", details: error });
+  }
+});
+
+app.post("/fire-calculator", authMiddleware, async (req, res) => {
+  try {
+    const {
+      lifestyleExpenses,
+      retirementAge,
+      currentSavings,
+      annualSavings,
+      userId,
+    } = req.body;
+    const newCalculator = await FireCalculator.create({
+      lifestyleExpenses,
+      retirementAge,
+      currentSavings,
+      annualSavings,
+      userId,
+    });
+    res.status(201).json(newCalculator);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create record", details: error });
+  }
+});
+
+app.put("/fire-calculator/:userId", authMiddleware, async (req, res) => {
+  try {
+    const updatedCalculator = await FireCalculator.update(req.body, {
+      where: { userId: req.params.userId },
+    });
+    res.status(200).json(updatedCalculator);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update record", details: error });
+  }
+});
+
+app.delete("/fire-calculator/:userId", authMiddleware, async (req, res) => {
+  try {
+    const deleted = await FireCalculator.destroy({
+      where: { userId: req.params.userId },
+    });
+    res
+      .status(200)
+      .json({ message: "Record deleted successfully", count: deleted });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete record", details: error });
+  }
+});
+
+app.get("/goals/:userId", authMiddleware, async (req, res) => {
+  try {
+    const goals = await Goal.findAll({ where: { userId: req.params.userId } });
+    res.status(200).json(goals);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve goals", details: error });
+  }
+});
+
+app.post("/goals", authMiddleware, async (req, res) => {
+  try {
+    const { name, targetAmount, deadline, userId } = req.body;
+    const newGoal = await Goal.create({ name, targetAmount, deadline, userId });
+    res.status(201).json(newGoal);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create goal", details: error });
+  }
+});
+
+app.put("/goals/:id", authMiddleware, async (req, res) => {
+  try {
+    const updatedGoal = await Goal.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.status(200).json(updatedGoal);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update goal", details: error });
+  }
+});
+
+app.delete("/goals/:id", authMiddleware, async (req, res) => {
+  try {
+    const deleted = await Goal.destroy({ where: { id: req.params.id } });
+    res
+      .status(200)
+      .json({ message: "Goal deleted successfully", count: deleted });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete goal", details: error });
+  }
+});
+
+app.get("/contributions/:goalId", authMiddleware, async (req, res) => {
+  try {
+    const contributions = await Contribution.findAll({
+      where: { goalId: req.params.goalId },
+    });
+    res.status(200).json(contributions);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to retrieve contributions", details: error });
+  }
+});
+
+app.post("/contributions", authMiddleware, async (req, res) => {
+  try {
+    const { amount, date, goalId } = req.body;
+    const newContribution = await Contribution.create({ amount, date, goalId });
+    res.status(201).json(newContribution);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to create contribution", details: error });
+  }
+});
+
+app.put("/contributions/:id", authMiddleware, async (req, res) => {
+  try {
+    const updatedContribution = await Contribution.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.status(200).json(updatedContribution);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to update contribution", details: error });
+  }
+});
+
+app.delete("/contributions/:id", authMiddleware, async (req, res) => {
+  try {
+    const deleted = await Contribution.destroy({
+      where: { id: req.params.id },
+    });
+    res
+      .status(200)
+      .json({ message: "Contribution deleted successfully", count: deleted });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to delete contribution", details: error });
+  }
+});
+
+app.get("/investments/:userId", authMiddleware, async (req, res) => {
+  try {
+    const investments = await Investments.findAll({
+      where: { userId: req.params.userId },
+    });
+    res.status(200).json(investments);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to retrieve investments", details: error });
+  }
+});
+
+app.post("/investments", authMiddleware, async (req, res) => {
+  try {
+    const { name, status, type, amount, userId } = req.body;
+    const newInvestment = await Investments.create({
+      name,
+      status,
+      type,
+      amount,
+      userId,
+    });
+    res.status(201).json(newInvestment);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to create investment", details: error });
+  }
+});
+
+app.put("/investments/:id", authMiddleware, async (req, res) => {
+  try {
+    const updatedInvestment = await Investments.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.status(200).json(updatedInvestment);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to update investment", details: error });
+  }
+});
+
+app.delete("/investments/:id", authMiddleware, async (req, res) => {
+  try {
+    const deleted = await Investments.destroy({ where: { id: req.params.id } });
+    res
+      .status(200)
+      .json({ message: "Investment deleted successfully", count: deleted });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to delete investment", details: error });
+  }
+});
+
+app.get("/notifications/:userId", authMiddleware, async (req, res) => {
+  try {
+    const notifications = await Notification.findAll({
+      where: { userId: req.params.userId },
+    });
+    res.status(200).json(notifications);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to retrieve notifications", details: error });
+  }
+});
+
+app.post("/notifications", authMiddleware, async (req, res) => {
+  try {
+    const { title, due_date, amount_due, status, userId } = req.body;
+    const newNotification = await Notification.create({
+      title,
+      due_date,
+      amount_due,
+      status,
+      userId,
+    });
+    res.status(201).json(newNotification);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to create notification", details: error });
+  }
+});
+
+app.put("/notifications/:id", authMiddleware, async (req, res) => {
+  try {
+    const updatedNotification = await Notification.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.status(200).json(updatedNotification);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to update notification", details: error });
+  }
+});
+
+app.delete("/notifications/:id", authMiddleware, async (req, res) => {
+  try {
+    const deleted = await Notification.destroy({
+      where: { id: req.params.id },
+    });
+    res
+      .status(200)
+      .json({ message: "Notification deleted successfully", count: deleted });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to delete notification", details: error });
+  }
+});
+
 // Sync models and start server
 sequelize
   .sync()
